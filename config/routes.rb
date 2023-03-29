2818ab8f761b651f73b3673c7fc26e708956783c
dorian fiolet <dorian3012@hotmail.fr>
2023-03-29 19:51:13 +0200

    addBookings

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   get "dashboard", to: "pages#dashboard"
 
-  resources :pokemons
+  resources :pokemons do
+    resources :bookings, only: [:create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
